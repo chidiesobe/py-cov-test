5f70885ae70b2965828c493cd11b8761e2ceb0b7
Chidi E <chidiesobe@gmail.com>
2025-02-06 09:13:14 +0000

    Fix: returned the substract function

--- calculator.py
+++ calculator.py
@@ -1,8 +1,8 @@
 def add(a: float, b: float) -> float:
     return a + b
 
-# def subtract(a: float, b: float) -> float:
-#     return a - b
+def subtract(a: float, b: float) -> float:
+    return a - b
 
 def multiply(a: float, b: float) -> float:
     return a * b
